e2a5399898599c039256e4cc8f189d6af51b3bd5
theo m <theo@mantz.io>
2021-04-26 21:43:52 -0400

    Write scraping functions, firebase backend

--- app.js
+++ app.js
@@ -1,4 +1,35 @@
 const express = require('express');
 const app = express();
+import {
+  getSubreddits
+} from './web_scrapers/subredditScraper'
 const path = require('path');
 const axios = require('axios');
+const admin = require('firebase-admin');
+const serviceAccount = require('./config/firebase_credentials.json');
+
+admin.initializeApp({
+  credential: admin.credential.cert(serviceAccount),
+  databaseURL: "https://drivingdoge-default-rtdb.firebaseio.com",
+});
+
+
+const db = admin.firestore();
+app.post('subreddits/:subreddit', (req, res) => {
+  console.log(req.params)
+  const subredditsObject = getSubreddits(req.params.subreddit)
+  const subReddits = db.collection('subreddits')
+  Object.keys(subredditsObject).forEach(subreddit => {
+    subReddits.doc(subreddit).set(subReddits[subreddit], { merge: true })
+      .then( () => res.status(200).json('success'))
+      .catch( err => res.status(400).json(err))
+  })
+})
+
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(PORT, () => {
+  console.log(__dirname)
+  console.log(`listening on ${PORT}`)
+})
---
+++ config/params.js
@@ -0,0 +1,6 @@
+
+
+export const twitterParams = {
+  max_results: "100",
+  tweet_fields: "public_metrics,entities,geo,lang",
+};
