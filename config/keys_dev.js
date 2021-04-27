003b6c4956d17897d7036f87cfb57408d1002fb9
theo m <theo@mantz.io>
2021-04-27 10:52:00 -0400

    Remove keys dev

--- config/keys_dev.js
+++
@@ -1,5 +0,0 @@
-module.exports = {
-  mongoURI:
-    "mongodb+srv://Theo-dev:RTbTl9oGYdbyBuAG@cluster0.puobv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
-  secretOrKey: "1wkkrZgCKDB1ntOAsLleZPmamvsSAGFE",
-};
