9af2211b8a5311581101fe3a30b51282a08bbebd
theo m <theo@mantz.io>
2021-04-27 10:50:29 -0400

    Update config, functions

---
+++ config/keys_dev.js
@@ -0,0 +1,5 @@
+module.exports = {
+  mongoURI:
+    "mongodb+srv://Theo-dev:RTbTl9oGYdbyBuAG@cluster0.puobv.mongodb.net/myFirstDatabase?retryWrites=true&w=majority",
+  secretOrKey: "1wkkrZgCKDB1ntOAsLleZPmamvsSAGFE",
+};
