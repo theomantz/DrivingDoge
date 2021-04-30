e0d4000e095e1682d232edbb02d59b6e25d0e33d
theo m <theo@mantz.io>
2021-04-30 19:14:56 -0400

    Add tensorflow

---
+++ config/urls.js
@@ -0,0 +1,6 @@
+module.exports = {
+  metadata:
+    "/Users/theo/Desktop/aA/DrivingDoge/tensorflow/resources/metadata.json",
+  model: 
+    "/Users/theo/Desktop/aA/DrivingDoge/tensorflow/resources/model.json",
+};
