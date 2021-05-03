3fee5cabb7131077df6041e6ca5fc7c93656ae94
theo m <theo@mantz.io>
2021-05-03 10:31:16 -0400

    Update keys

--- config/keys.js
+++ config/keys.js
@@ -2,5 +2,5 @@
 if (process.env.NODE_ENV === "production") {
   module.exports = require("./keys_prod");
 } else {
-  module.exports = require("./keys_dev").mongoDB.mongoURI;
+  module.exports = require("./keys_dev");
 }
