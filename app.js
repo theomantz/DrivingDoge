4934e30d2cfc7f4e73d049cf6eb3a1601b8c6e58
theo m <theo@mantz.io>
2021-05-14 09:40:12 -0400

    Add new relic rpm

--- app.js
+++ app.js
@@ -1,55 +1,53 @@
-const express = require('express');
+require("newrelic");
+const express = require("express");
 const app = express();
-const path = require('path');
-
+const path = require("path");
 
 // Depreciated scraper in favor of 'constructXByY' architecture
 // const getSubreddits = require('./web_scrapers/subredditScraper')
 
 // Scraper utilities
-const constructSubredditsByQuery = require('./util/subredditUtil')
-const constructPostsBySubreddit = require('./util/postUtil')
-const constructCommentsByPost = require('./util/commentUtil')
+const constructSubredditsByQuery = require("./util/subredditUtil");
+const constructPostsBySubreddit = require("./util/postUtil");
+const constructCommentsByPost = require("./util/commentUtil");
 
 // Response utility
-const constructQueryForResponse = require('./util/queryUtil')
+const constructQueryForResponse = require("./util/queryUtil");
 
 // Query API
-const query = require('./routes/api/data')
+const query = require("./routes/api/data");
 
-app.use('/api/asset', query)
+app.use("/api/asset", query);
 
 // Mongoose models
-const Query = require('./models/Query');
+const Query = require("./models/Query");
 
 // Validators
-const validateQueryInput = require('./validation/query');
+const validateQueryInput = require("./validation/query");
 
-app.use(express.static('public'))
+app.use(express.static("public"));
 
-app.get('/', (req, res) => {
-  console.log('request', req)
-  res.sendFile(path.join(__dirname, './public/index.html'))
-})
+app.get("/", (req, res) => {
+  console.log("request", req);
+  res.sendFile(path.join(__dirname, "./public/index.html"));
+});
 
 // Database Setup
 
-const mongoose = require('mongoose');
-const db = require('./config/keys').mongoURI;
-
+const mongoose = require("mongoose");
+const db = require("./config/keys").mongoURI;
 
 mongoose
-  .connect(db, { 
-    useNewUrlParser: true, 
-    useUnifiedTopology: true, 
-    useFindAndModify: false 
+  .connect(db, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useFindAndModify: false,
   })
   .then(() => console.log("Connected to MongoDB Successfully"))
   .catch((err) => console.log(err));
 
 // Singular Route: builds query object, passes queryObject to each util
 // Returns completed queryDoc
-
 
 /* app.post("/query/:query", async (req, res) => {
   
@@ -97,6 +95,5 @@ mongoose
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
-
-  console.log(`listening on ${PORT}`)
-})
+  console.log(`listening on ${PORT}`);
+});
